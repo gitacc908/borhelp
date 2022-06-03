66ac091595799db3cb898cc11dea06f961ef4df4
gitacc908 <dylan9803@mail.ru>
2022-06-03 18:07:45 +0600

    icons

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG')
-DEBUG = 0
+DEBUG = 1
 # ALLOWED_HOSTS = ['0.0.0.0', '159.69.247.34', 'dteam.foundation']
 
 ALLOWED_HOSTS = ['*']
